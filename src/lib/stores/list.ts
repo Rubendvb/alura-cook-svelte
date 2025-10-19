d5a5f9402e7e80cf6b468152e72f3bb1d01f785a
Rubendvb <rubenvasquez556@gmail.com>
2025-10-19 18:00:04 -0300

    Enhance arrayList store with methods for adding and removing ingredients, improving state management in the application.

--- src/lib/stores/list.ts
+++ src/lib/stores/list.ts
@@ -1,3 +1,15 @@
 import { writable } from 'svelte/store';
 
-export const arrayList = writable<string[]>([]);
+const { subscribe, set, update } = writable<string[]>([]);
+
+export const arrayList = {
+	subscribe,
+	set,
+	addingIngredient(ingredient: string) {
+		update((currentValue) => [...currentValue, ingredient]);
+	},
+
+	removeIngredient(ingredient: string) {
+		update((currentValue) => currentValue.filter((item) => item !== ingredient));
+	}
+};
